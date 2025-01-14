1c511458b3055454b44505b9944661ff932b050e
Francis Awoh <realmabove02@gmail.com>
2025-01-14 05:24:07 -0800

    added login function

--- quizProject/quizApp/views.py
+++ quizProject/quizApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib import messages
 
 # Registration view
@@ -11,6 +11,18 @@ def register(request):
       user = form.save()
       login(request, user)
       messages.success(request, 'Registration Successful!')
-      return redirect('home_page')
+      return redirect('login')
+    else:
+      messages.error(request, 'Error in Registration')
+  else:
+    form = UserRegistrationForm()
+  return render(request, 'quizApp/register.html', {'form' : form})
+
+# login the user view
+def login(request):
+  if request.method == 'POST':
+    form = UserLoginForm(request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
 
 
