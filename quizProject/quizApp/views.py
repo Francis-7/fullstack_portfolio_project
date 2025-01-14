daad31a9df3c46aa6f7ed753ce7d071f8bfc8428
Francis Awoh <realmabove02@gmail.com>
2025-01-14 06:12:19 -0800

    imported login required from django.contrib.auth.decorators

--- quizProject/quizApp/views.py
+++ quizProject/quizApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Registration view
 def register(request):
@@ -37,7 +38,6 @@ def login(request):
   else:
     form = UserLoginForm()
   return render(request, 'quizApp/login.html', {'form' : form})
-
 
 
 
