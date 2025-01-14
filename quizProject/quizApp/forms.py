adcb6d81a9415eb21ec1b84e6a0efa3ed705baef
Francis Awoh <realmabove02@gmail.com>
2025-01-14 04:58:38 -0800

    created the register form and the login form inside the forms module

---
+++ quizProject/quizApp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import UserProfile
+
+class UserRegistrationForm(UserCreationForm):
+  first_name = forms.CharField(max_length=40)
+  last_name = forms.CharField(max_length=40)
+  email = forms.EmailField(required=True)
+
+  class Meta:
+    model = User
+    fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+  def save(self, commit=True):
+    user = super().save(commit=False)
+    if commit:
+      user.save()
+      UserProfile.objects.create(user=user)
+    return user
+  
+
+class UserLoginForm(AuthenticationForm):
+  username = forms.CharField(max_length=50)
+  password = forms.CharField(widget=forms.PasswordInput)
