618aafb7f1fa30123fef5fec40d6b7a81b2a1001
Francis Awoh <realmabove02@gmail.com>
2025-01-14 00:49:38 -0800

    imported reverse an format_html in admin module for better formatting of choice models in admin

--- quizProject/quizApp/admin.py
+++ quizProject/quizApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Question, Choice
+from django.urls import reverse
+from django.utils.html import format_html
 
 # Register your models here.
 @admin.register(Question)
@@ -8,4 +10,8 @@ class QuestionAdmin(admin.ModelAdmin):
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-  list_display = ['question', 'choice', 'answer_to_question']
+  list_display = ['choice', 'answer_to_question', 'question_tag']
+
+  def question_tag(self, obj):
+    question = obj.question_set.all()
+    if len(question) == 0:
