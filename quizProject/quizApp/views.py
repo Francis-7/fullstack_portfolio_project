8be9265e6550f22351547a5628558b16424bc112
Francis Awoh <realmabove02@gmail.com>
2025-01-14 05:53:34 -0800

    added messages.success

--- quizProject/quizApp/views.py
+++ quizProject/quizApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib import messages
 
@@ -24,5 +24,10 @@ def login(request):
     form = UserLoginForm(request.POST)
     if form.is_valid():
       username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+        login(request, user)
+        messages.success(request, 'Login Successful')
 
 
