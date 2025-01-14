706101efde4f5fe4c1990e09bd053d050a323a26
Francis Awoh <realmabove02@gmail.com>
2025-01-14 00:28:13 -0800

    edited ordering to question and choice also added default value for foreign key to question.question

--- quizProject/quizApp/models.py
+++ quizProject/quizApp/models.py
@@ -13,14 +13,14 @@ class Question(models.Model):
       return self.question
     
 class Choice(models.Model):
-  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="all_questions")
+  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="all_questions", default='{question.question}')
   Options = models.TextChoices('Options', 'A B C D')
   choice = models.CharField(max_length=1, choices=Options)
   answer_to_question = models.TextField(unique=True, default="your answer")
 
   class Meta:
     db_table = 'choices'
-    ordering = ['choice']
+    ordering = ['question', 'choice']
 
     def __str__(self):
       return f"{self.choice}: {self.answer_to_question}"
