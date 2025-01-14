c884ccd93e0300bcdddbb1b2a24f116c010dd75d
Francis Awoh <realmabove02@gmail.com>
2025-01-13 22:11:16 -0800

    created models question and choices

--- quizProject/quizApp/models.py
+++ quizProject/quizApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+  question_num = models.IntegerField(primary_key=True)
+  question = models.TextField(unique=True)
+  author = models.CharField(max_length=30)
+
+  class Meta:
+    db_table = 'questions'
+    ordering = ['question_num']
+
+    def __str__(self):
+      return f"Question {self.question_num} by {self.author}"
+    
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="all_questions")
+  Options = models.TextChoices('Options', 'A B C D')
+  choice = models.CharField(max_length=1, choices=Options, unique=True)
+  
