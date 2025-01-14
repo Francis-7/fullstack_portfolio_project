bfaf81c2e1e75eebfd6edf3ce84df7f0aa088d64
Francis Awoh <realmabove02@gmail.com>
2025-01-14 05:07:03 -0800

    defined register function

--- quizProject/quizApp/views.py
+++ quizProject/quizApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
+from django.contrib import messages
 
-# Create your views here.
+# Registration view
+def register(request):
+  if request.method == 'POST':
+    form = UserRegistrationForm(request.POST)
+    
