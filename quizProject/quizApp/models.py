dedb6bddf463e3a26b139a7f88c2a2c3b1a93392
Francis Awoh <realmabove02@gmail.com>
2025-01-15 02:04:14 -0800

    added fields name description and created_at

--- quizProject/quizApp/models.py
+++ quizProject/quizApp/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from datetime import timedelta
 
 class Quiz(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
   pass
 
 class Question(models.Model):
