29ff4ad5f42fd396dae2111a4c2702fc2a56c893
Francis Awoh <realmabove02@gmail.com>
2025-01-14 23:57:31 -0800

    import timezone from django.utils

--- quizProject/quizApp/views.py
+++ quizProject/quizApp/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import QuestionSerializer
+from django.utils import timezone
 
 # Registration view
 def register(request):
