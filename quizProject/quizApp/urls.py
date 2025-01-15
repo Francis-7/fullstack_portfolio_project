e09f196a1d96c2323f2fade1e4bb858a33ffdad8
Francis Awoh <realmabove02@gmail.com>
2025-01-15 01:44:30 -0800

    created a urls for jwt views token

--- quizProject/quizApp/urls.py
+++ quizProject/quizApp/urls.py
@@ -2,5 +2,5 @@ from rest_framework_simplejwt import views as jwt_views
 from django.urls import path
 
 urlpatterns = [
-  path('api/token/')
+  path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
