da307d1c98ee6b31d59f0f2bc286b365336060a6
Francis Awoh <realmabove02@gmail.com>
2025-01-15 01:45:31 -0800

    create urls for refresh token

--- quizProject/quizApp/urls.py
+++ quizProject/quizApp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
   path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
