ae2fc9ff0a9b3cb26d7d08c59ef87799d92f785c
Francis Awoh <realmabove02@gmail.com>
2025-01-15 01:46:38 -0800

    created urls for the question api

--- quizProject/quizApp/urls.py
+++ quizProject/quizApp/urls.py
@@ -1,7 +1,9 @@
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt import views
 from django.urls import path
 
 urlpatterns = [
   path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+  path('api/questions/', views.QuestionListView.as_view(), name='question-list'),
 ]
