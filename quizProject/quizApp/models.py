6131ecfbf15321ec53286516fcadee2464589dc8
Francis Awoh <realmabove02@gmail.com>
2025-01-15 00:58:24 -0800

    import timedelta from datetime

--- quizProject/quizApp/models.py
+++ quizProject/quizApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from datetime import timedelta
 
 class Question(models.Model):
   question_num = models.IntegerField(primary_key=True)
